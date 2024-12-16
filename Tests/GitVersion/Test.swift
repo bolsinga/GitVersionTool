960c9d4ee585d653de187c462dc2e0cae34acaeb
Greg Bolsinga <bolsinga@mac.com>
2024-12-16 09:26:59 -0800

    fallback to branch name when there is not a tag

--- Tests/GitVersion/Test.swift
+++ Tests/GitVersion/Test.swift
@@ -32,4 +32,16 @@ struct Test {
   @Test func state_tag() throws {
     #expect("\(Report(state: .noChanges, tag: "name"))" == "name")
   }
+
+  @Test func state_branch() throws {
+    #expect("\(Report(state: .noChanges, tag: "tag", branch: "branch"))" == "tag")
+    #expect("\(Report(state: .noChanges, tag: nil, branch: "branch"))" == "branch")
+    #expect("\(Report(state: .noChanges, tag: "", branch: "branch"))" == "branch")
+  }
+
+  @Test func state_branchEmpty() throws {
+    #expect("\(Report(state: .noChanges, tag: "tag", branch: ""))" == "tag")
+    #expect("\(Report(state: .noChanges, tag: nil, branch: ""))" == "version")
+    #expect("\(Report(state: .noChanges, tag: "", branch: ""))" == "version")
+  }
 }
