9e144a262183a21b6de49b355f31459acfbec815
Greg Bolsinga <bolsinga@mac.com>
2024-12-16 10:01:37 -0800

    test the base enum and then variants simply

--- Tests/GitVersion/Test.swift
+++ Tests/GitVersion/Test.swift
@@ -11,15 +11,19 @@ import Testing
 @testable import GitVersion
 
 struct Test {
-  @Test func state_noTag() throws {
-    #expect("\(Report(state: .notGitDirectory))" == "unknown")
-    #expect("\(Report(state: .localChanges))" == "local")
-    #expect("\(Report(state: .noChanges))" == "version")
+  @Test func state_noName() throws {
+    #expect("\(Report(state: .notGitDirectory, name: nil))" == "unknown")
+    #expect("\(Report(state: .localChanges, name: nil))" == "local")
+    #expect("\(Report(state: .noChanges, name: nil))" == "version")
+  }
+
+  @Test func state_name() throws {
+    #expect("\(Report(state: .notGitDirectory, name: "name"))" == "unknown")
+    #expect("\(Report(state: .localChanges, name: "name"))" == "name-local")
+    #expect("\(Report(state: .noChanges, name: "name"))" == "name")
   }
 
   @Test func state_tag() throws {
-    #expect("\(Report(state: .notGitDirectory, tag: "tag"))" == "unknown")
-    #expect("\(Report(state: .localChanges, tag: "tag"))" == "tag-local")
-    #expect("\(Report(state: .noChanges, tag: "tag"))" == "tag")
+    #expect("\(Report(state: .noChanges, tag: "name"))" == "name")
   }
 }
