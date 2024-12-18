2e97b9135febba370d6e56cc7fb9851a51b2419c
Greg Bolsinga <bolsinga@mac.com>
2024-12-18 08:15:22 -0800

    add log statments about what reportable name is used. (#30)

--- Sources/GitVersion/Reportable+Name.swift
+++ Sources/GitVersion/Reportable+Name.swift
@@ -5,6 +5,14 @@
 //  Created by Greg Bolsinga on 12/17/24.
 //
 
+import Foundation
+import os
+
+extension Logger {
+  fileprivate static let name = Logger(
+    subsystem: Bundle.main.bundleIdentifier ?? "unknown", category: "name")
+}
+
 extension Reportable {
   func name() async -> String? {
     guard let name = await tag(), !name.isEmpty else {
@@ -12,10 +20,13 @@ extension Reportable {
         guard let name = await commit(), !name.isEmpty else {
           return nil
         }
+        Logger.name.info("commit: \(name, privacy: .public)")
         return name
       }
+      Logger.name.info("branch: \(name, privacy: .public)")
       return name
     }
+    Logger.name.info("tag: \(name, privacy: .public)")
     return name
   }
 }
